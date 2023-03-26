8f323ab74f24c25ba90d745b6174d902dcf3c6d7
F3FFO <federico.pieranton@gmail.com>
2023-03-26 16:58:27 +0200

    Update script for module creation

--- create-module.sh
+++ create-module.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 MODULE_NAME=open-webview.zip
+TOOLS=tools.tar.xz
 
-rm -r $MODULE_NAME
-zip -r $MODULE_NAME ./* -x ./git/\* ./.github/\* ./img/\* ./common/tools/tools/\* ./README.md\* ./CHANGELOG.md\* ./gitignore\*
+echo "deleting old files..."
+rm -rf $MODULE_NAME
+rm -rf common/tools/$TOOLS
+echo -e "ok!\n\nzipping tools..."
+tar -cJf common/tools/$TOOLS common/tools/tools
+echo -e "ok!\n\ncreating module zip..."
+7z a -tzip -r $MODULE_NAME ./* -x!./git/\* -x!./.github/\* -x!./img/\* -x!./common/tools/tools/\* -x!./README.md\* -x!./CHANGELOG.md\* -x!./gitignore\* >& /dev/null
+echo "done!"
