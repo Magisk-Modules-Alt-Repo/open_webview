542cb981fed2308b975e2080a90a0f2df3260e0f
F3FFO <federico.pieranton@gmail.com>
2023-03-26 16:57:27 +0200

    update installation script

--- common/install.sh
+++ common/install.sh
@@ -2,18 +2,16 @@
 MANUFACTER=$(getprop ro.product.manufacturer)
 ABI=$(grep_prop ro.product.cpu.abi)
 STATUS=0
+BROMITE_VERSION=108.0.5359.109
+BROMITE_URL=https://github.com/bromite/bromite/releases/download/${BROMITE_VERSION}/${ARCH}_SystemWebView.apk
+BROMITE_SHA_URL=https://github.com/bromite/bromite/releases/download/${BROMITE_VERSION}/brm_${BROMITE_VERSION}.sha256.txt
+BROMITE_APK_FILE="BromiteWebview.apk"
+BROMITE_SHA_FILE=brm_${BROMITE_VERSION}.sha256.txt
+BROMITE_NAME="Bromite"
+BROMITE_PACKAGE_NAME="org.bromite.webview"
+BROMITE_SYSTEM_PATH=system/app/BromiteWebview
+OVERLAY_APK_FILE="WebviewOverlay.apk"
 
-bromite() {
-	BROMITE_VERSION=106.0.5249.163
-	BROMITE_DESCRIPTION_NAME="Bromite"
-	BROMITE_PACKAGE_NAME="org.bromite.webview"
-	BROMITE_APK_FILE="BromiteWebview.apk"
-	BROMITE_OVERLAY_APK_FILE="WebviewOverlay.apk"
-	BROMITE_SHA_FILE=brm_${BROMITE_VERSION}.sha256.txt
-	BROMITE_URL=https://github.com/bromite/bromite/releases/download/${BROMITE_VERSION}/${ARCH}_SystemWebView.apk
-	BROMITE_SHA_URL=https://github.com/bromite/bromite/releases/download/${BROMITE_VERSION}/brm_${BROMITE_VERSION}.sha256.txt
-	BROMITE_SYSTEM_PATH=system/app/${BROMITE_DESCRIPTION_NAME}Webview
-}
 download_file() {
 	STATUS = 0
 	ui_print "  Downloading ${1}..."
@@ -45,27 +43,32 @@ check_integrity() {
 	fi
 }
 extract_lib() {
-	mktouch "$MODPATH"/$BROMITE_SYSTEM_PATH/.placeholder
+	#mktouch "$MODPATH"/$BROMITE_SYSTEM_PATH/.placeholder
+	mkdir -p "$MODPATH"/$BROMITE_SYSTEM_PATH
 	cp_ch "$TMPDIR"/$BROMITE_APK_FILE "$MODPATH"/$BROMITE_SYSTEM_PATH/webview.apk
-	cp_ch "$TMPDIR"/$BROMITE_APK_FILE "$TMPDIR"/bromite_webview.zip
-	mkdir -p "$TMPDIR"/bromite_webview "$MODPATH"/$BROMITE_SYSTEM_PATH/lib/arm64 "$MODPATH"/$BROMITE_SYSTEM_PATH/lib/arm
-	unzip -qo "$TMPDIR"/bromite_webview.zip -d "$TMPDIR"/bromite_webview >&2
-	cp_ch "$TMPDIR"/bromite_webview/lib/arm64-v8a/* "$MODPATH"/$BROMITE_SYSTEM_PATH/lib/arm64
-	cp_ch "$TMPDIR"/bromite_webview/lib/armeabi-v7a/* "$MODPATH"/$BROMITE_SYSTEM_PATH/lib/arm
+	cp_ch "$TMPDIR"/$BROMITE_APK_FILE "$TMPDIR"/"${BROMITE_NAME}Webview.zip"
+	mkdir -p "$TMPDIR"/"${BROMITE_NAME}Webview" "$MODPATH"/$BROMITE_SYSTEM_PATH/lib/arm64 "$MODPATH"/$BROMITE_SYSTEM_PATH/lib/arm
+	unzip -qo "$TMPDIR"/"${BROMITE_NAME}Webview.zip" -d "$TMPDIR"/"${BROMITE_NAME}Webview" >&2
+	cp_ch "$TMPDIR"/"${BROMITE_NAME}Webview"/lib/arm64-v8a/* "$MODPATH"/$BROMITE_SYSTEM_PATH/lib/arm64
+	cp_ch "$TMPDIR"/"${BROMITE_NAME}Webview"/lib/armeabi-v7a/* "$MODPATH"/$BROMITE_SYSTEM_PATH/lib/arm
 }
 create_overlay_min_api_29() {
-	sed -i "s/__webview-name__/${BROMITE_DESCRIPTION_NAME}/g" "$MODPATH"/common/overlay29/res/xml/config_webview_packages.xml
+	sed -i "s/__webview-name__/${BROMITE_NAME}/g" "$MODPATH"/common/overlay29/res/xml/config_webview_packages.xml
 	sed -i "s/__webview-package__/${BROMITE_PACKAGE_NAME}/g" "$MODPATH"/common/overlay29/res/xml/config_webview_packages.xml
-	aapt p -f -v -M "$MODPATH"/common/overlay29/AndroidManifest.xml -I /system/framework/framework-res.apk -S "$MODPATH"/common/overlay29/res -F "$MODPATH"/unsigned.apk >&2
+	if [ $API -ge 30 ]; then
+		aapt2 compile -o "$MODPATH"/unsigned.apk -v "$MODPATH"/common/overlay29/AndroidManifest.xml -I /system/framework/framework-res.apk "$MODPATH"/common/overlay29/res >&2
+	else
+		aapt p -f -v -M "$MODPATH"/common/overlay29/AndroidManifest.xml -I /system/framework/framework-res.apk -S "$MODPATH"/common/overlay29/res -F "$MODPATH"/unsigned.apk >&2
+	fi
 }
-create_overlay_max_api_29() {
-	sed -i "s/__webview-name__/${BROMITE_DESCRIPTION_NAME}/g" "$MODPATH"/common/overlay28/res/xml/config_webview_packages.xml
+create_overlay_max_api_28() {
+	sed -i "s/__webview-name__/${BROMITE_NAME}/g" "$MODPATH"/common/overlay28/res/xml/config_webview_packages.xml
 	sed -i "s/__webview-package__/${BROMITE_PACKAGE_NAME}/g" "$MODPATH"/common/overlay28/res/xml/config_webview_packages.xml
 	aapt p -f -v -M "$MODPATH"/common/overlay28/AndroidManifest.xml -I /system/framework/framework-res.apk -S "$MODPATH"/common/overlay28/res -F "$MODPATH"/unsigned.apk >&2
 }
 sign_framework_res() {
 	sign "$MODPATH"/unsigned.apk "$MODPATH"/signed.apk
-	cp -rf "$MODPATH"/signed.apk "$MODPATH"/common/$BROMITE_OVERLAY_APK_FILE
+	cp -rf "$MODPATH"/signed.apk "$MODPATH"/common/$OVERLAY_APK_FILE
 	rm -rf "$MODPATH"/signed.apk "$MODPATH"/unsigned.apk
 }
 find_overlay_path() {
@@ -83,24 +86,23 @@ find_overlay_path() {
 }
 force_overlay() {
 	mkdir -p "$MODPATH"/$OVERLAY_PATH
-	cp_ch "$MODPATH"/common/$BROMITE_OVERLAY_APK_FILE "$MODPATH"/$OVERLAY_PATH
+	cp_ch "$MODPATH"/common/$OVERLAY_APK_FILE "$MODPATH"/$OVERLAY_PATH
 }
 clean_up() {
 	if [ ${1} -eq 1 ]; then
 		ui_print "  Cleaning up..."
-		rm -rf "$MODPATH"/$BROMITE_SYSTEM_PATH/.placeholder
+		#rm -rf "$MODPATH"/$BROMITE_SYSTEM_PATH/.placeholder
 		ui_print "  !!! Dalvik cache will be cleared next boot !!!"
 		ui_print "  !!! Boot time may be longer !!!"
 	else
 		ui_print ""
 		ui_print "  Installation failed."
 		ui_print "  Cleaning up..."
-		rm -rf "$MODPATH"/$BROMITE_SYSTEM_PATH "$MODPATH"/$OVERLAY_PATH
+		#rm -rf "$MODPATH"/$BROMITE_SYSTEM_PATH "$MODPATH"/$OVERLAY_PATH
 		exit 1
 	fi
 }
 
-bromite
 ui_print "  Detecting architecture..."
 ui_print "  CPU architecture: ${ARCH}."
 download_file $BROMITE_APK_FILE $BROMITE_URL
@@ -115,7 +117,7 @@ ui_print "    creating overlays..."
 if [ $API -ge 29 ]; then
 	create_overlay_min_api_29
 else
-	create_overlay_max_api_29
+	create_overlay_max_api_28
 fi
 if [ -f "${MODPATH}/unsigned.apk" ]; then
 	sign_framework_res
@@ -133,7 +135,7 @@ find_overlay_path
 if [ $STATUS -eq 1 ]; then
 	force_overlay
 fi
-if [ ! -f "${MODPATH}/$OVERLAY_PATH/$BROMITE_OVERLAY_APK_FILE" ]; then
+if [ ! -f "${MODPATH}/$OVERLAY_PATH/$OVERLAY_APK_FILE" ]; then
 	STATUS=0
 fi
 
