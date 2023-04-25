5528f95c05b26f442e9f4330a815c48a127128ce
F3FFO <federico.pieranton@gmail.com>
2023-04-26 00:18:48 +0200

    Use double brackets

--- service.sh
+++ service.sh
@@ -4,8 +4,8 @@ CONFIG_FILE="$MODDIR/.webview"
 VW_PACKAGE=$(grep "VW_PACKAGE=" ${CONFIG_FILE} | cut -d"=" -f2)
 
 PROPFILE="$MODDIR/module.prop"
-if [ -n "$(ls -a /data/misc/shared_relro)" ]; then
-    if [ "pm list packages -a | grep -q ${VW_PACKAGE}" ]; then
+if [[ -n "$(ls -a /data/misc/shared_relro)" ]]; then
+    if [[ "pm list packages -a | grep -q ${VW_PACKAGE}" ]]; then
         sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ ✅ Module is working ] /g' "$PROPFILE"
     fi
 else
