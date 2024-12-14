846c82356f013281cc131b61dab4fd38bdcf2fe7
F3FFO <federico.pieranton@gmail.com>
2024-12-14 20:21:29 +0100

    Update shell script create-module

--- create-module.sh
+++ create-module.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
-MODULE_NAME=open-webview.zip
-TOOLS=tools.tar
+set -e
+
+MODULE_NAME="open-webview.zip"
+TOOLS="tools.tar"
 ROOT_DIR="$(pwd)"
+OVERLAY_DIR="overlays"
+TOOLS_DIR="common/tools"
+
+compress() {
+    local type="$1"
+    local output="$2"
+    shift 2
+    local input=("$@")
+    echo "compressing $output..."
+    7z a -t"$type" -r "$output" "${input[@]}" >& /dev/null
+}
 
 echo "deleting old files..."
-rm -rf $MODULE_NAME
-rm -rf common/tools/"${TOOLS}.xz"
-echo -e "ok!\n\nzipping tools..."
-cd common/tools/tools
-7z a -ttar -r $TOOLS * >& /dev/null
-7z a -txz -r "${TOOLS}.xz" $TOOLS >& /dev/null
-rm -rf $TOOLS
+rm -f "$MODULE_NAME"
+rm -f "$TOOLS_DIR/${TOOLS}.xz"
+
+echo "zipping tools..."
+cd "$TOOLS_DIR/tools"
+compress tar "$TOOLS" *
+compress xz "${TOOLS}.xz" "$TOOLS"
+rm -f "$TOOLS"
 mv -f "${TOOLS}.xz" ../
-cd $ROOT_DIR
-echo -e "ok!\n\ncreating module zip..."
+cd "$ROOT_DIR"
+
+echo "Zipping overlays..."
+cd "$OVERLAY_DIR"
+rm -f *.zip
+
+declare -A overlays=(
+    ["cromite-overlay29.zip"]="./extracted/cromite-overlay29/*"
+    ["mulch-overlay28.zip"]="./extracted/mulch-overlay28/*"
+    ["mulch-overlay29.zip"]="./extracted/mulch-overlay29/*"
+    ["thorium-overlay29.zip"]="./extracted/thorium-overlay29/*"
+    ["vanadium-overlay29.zip"]="./extracted/vanadium-overlay29/*"
+)
+
+for zip_name in "${!overlays[@]}"; do
+    compress zip "$zip_name" "${overlays[$zip_name]}"
+done
+
+cd "$ROOT_DIR"
+echo -e "creating module zip..."
 7z a -tzip -r $MODULE_NAME * -xr!.git* -xr!img -xr!common/tools/tools -xr!overlays/extracted -x!*.md -x!create-module.* >& /dev/null
 echo "done!"
-
